10c1f57fa3817851437baaad7ff296a372f05b6a
yassinejemaa <jemmayassin@gmail.com>
2024-04-25 14:05:49 +0200

    ds3231
    waking up card  to do a task using ds3231

--- arduini_wakup_watchdog/arduini_wakup_watchdog.ino
+++ arduini_wakup_watchdog/arduini_wakup_watchdog.ino
@@ -1,67 +1,100 @@
-#include <avr/sleep.h>
-#include <avr/wdt.h>
+/*
+  ===========================================================================
+      Wake up from sleep example using DS3231 and Adafruit RTClib library
+  ===========================================================================
+  
+  Use the DS3231 RTC module to wake an µController from sleep using the output
+  from the SQW pin to trigger an interrupt driven wakeup. 
+  
+  In this example the µController goes to sleep after 10 seconds, sleeps for 10
+  seconds (set using alarm), wakes and repeats.
 
-int i = 0;
-volatile boolean wdt_triggered = false;  // Flag to indicate watchdog wake-up
-volatile boolean button_pressed = false; // Flag to indicate button press
+  Tested on a Arduino Nano. 3 July 2020.
+  
+  This example: https://github.com/garrysblog https://garrysblog.com/
+  Library: https://github.com/adafruit/RTClib
+  RTClib command info: https://adafruit.github.io/RTClib/html/class_r_t_c___d_s3231.html
+  
+  Connections
+  -----------
+  SDA -> Arduino Analog (SDA pin)
+  SCL -> Arduino Analog (SCL pin)
+  VCC -> Arduino 5V
+  GND -> Arduino GND
+  SQW -> Arduino D2 (Needs to be an interrupt capable pin)
+*/
 
-// Watchdog Timer Interrupt Service Routine
-ISR(WDT_vect) {
-  wdt_triggered = true;  // Set the wake-up flag
-}
+#include "RTClib.h"     // DS3231 library
+#include <avr/sleep.h>  // AVR library for controlling the sleep modes
 
-void buttonISR() {
-  button_pressed = true;  // Set the button press flag
-}
+const int alarmPin = 2; // The number of the pin for monitoring alarm status on DS3231
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(2, INPUT_PULLUP); // Set pin 2 as input with pull-up
+RTC_DS3231 rtc;
 
-  // Attach external interrupt to pin 2
-  attachInterrupt(digitalPinToInterrupt(2), buttonISR, FALLING);
+void setup () {
+  Serial.begin(9600); // Start serial port for monitoring
 
-  // Set up the Watchdog
-  wdt_reset();  // Reset the watchdog timer
-  WDTCSR = (1<<WDCE) | (1<<WDE);
-  WDTCSR = (1<<WDIE) | (1<<WDP3) | (1<<WDP0); // Enable interrupt mode, set timer to 8s
-}
-
-void loop() {
-  if (button_pressed || wdt_triggered) {
-    if (button_pressed) {
-      Serial.println("Woke up due to button press!");
-      button_pressed = false;
-      delay(300);
-    }
-    if (wdt_triggered) {
-      Serial.println("Woke up due to the Watchdog Timer!");
-      wdt_triggered = false;
-      delay(300);
-    }
-    
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-
-    i++;
-    if (i == 8) {
-      Serial.println("One minute passed");
-      i = 0;
-    } else {
-      
-    }
+  pinMode(alarmPin, INPUT_PULLUP); // Set alarm pin as pullup
+  
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
   }
 
-  goToSleep();
+  // If required set time
+  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // To compiled time
+  //rtc.adjust(DateTime(2020, 7, 3, 20, 0, 0)); // Or explicitly, e.g. July 3, 2020 at 8pm
+
+  // Disable and clear both alarms
+  rtc.disableAlarm(1);
+  rtc.disableAlarm(2);
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+
+  rtc.writeSqwPinMode(DS3231_OFF); // Place SQW pin into alarm interrupt mode
+
+  Serial.println("Starting");
 }
 
-void goToSleep() {
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Set the deepest sleep mode
-  sleep_enable();  // Enable sleep mode
-  sleep_mode();    // Go to sleep
+void loop () {
 
-  // The program will continue from here when the MCU wakes up
-  sleep_disable(); // Disable sleep mode after wake-up
+  delay(10000);  // Wait 10 seconds before going to sleep
+
+  // Get current time and set alarm to a time to wake
+  DateTime now = rtc.now();  // Get current time
+  rtc.setAlarm1(now + TimeSpan(0, 0, 0, 10), DS3231_A1_Second); // In 10 seconds
+  //rtc.setAlarm1(DateTime(2020, 6, 25, 15, 0, 0), DS3231_A1_Hour); // Or can be set explicity
+    
+  enterSleep();  // Go to sleep
+}
+
+void enterSleep(){
+  sleep_enable();                       // Enabling sleep mode
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);  // Setting the sleep mode, in this case full sleep
+  
+  noInterrupts();                       // Disable interrupts
+  attachInterrupt(digitalPinToInterrupt(alarmPin), alarm_ISR, LOW);
+  
+  Serial.println("Going to sleep!");    // Print message to serial monitor
+  Serial.flush();                       // Ensure all characters are sent to the serial monitor
+  
+  interrupts();                         // Allow interrupts again
+  sleep_cpu();                          // Enter sleep mode
+
+  /* The program will continue from here when it wakes */
+  
+  // Disable and clear alarm
+  rtc.disableAlarm(1);
+  rtc.clearAlarm(1);
+  
+  Serial.println("I'm back!");          // Print message to show we're back
+ }
+
+
+void alarm_ISR() {
+  // This runs when SQW pin is low. It will wake up the µController
+  
+  sleep_disable(); // Disable sleep mode
+  detachInterrupt(digitalPinToInterrupt(alarmPin)); // Detach the interrupt to stop it firing
 }
