c594060cd944474f42788bed7b903602380366a8
yassinejemaa <jemmayassin@gmail.com>
2024-04-27 10:51:40 +0200

    managed wakups (RTC ds3231+Button)
    the card wakes up every 10 seconds (even more) due to an external interruption , using RTC ds3231 external interruption,also it can be woken up with a button interruption (an external interruption also.

--- arduini_wakup_watchdog/arduini_wakup_watchdog.ino
+++ arduini_wakup_watchdog/arduini_wakup_watchdog.ino
@@ -1,100 +1,105 @@
-/*
-  ===========================================================================
-      Wake up from sleep example using DS3231 and Adafruit RTClib library
-  ===========================================================================
-  
-  Use the DS3231 RTC module to wake an µController from sleep using the output
-  from the SQW pin to trigger an interrupt driven wakeup. 
-  
-  In this example the µController goes to sleep after 10 seconds, sleeps for 10
-  seconds (set using alarm), wakes and repeats.
-
-  Tested on a Arduino Nano. 3 July 2020.
-  
-  This example: https://github.com/garrysblog https://garrysblog.com/
-  Library: https://github.com/adafruit/RTClib
-  RTClib command info: https://adafruit.github.io/RTClib/html/class_r_t_c___d_s3231.html
-  
-  Connections
-  -----------
-  SDA -> Arduino Analog (SDA pin)
-  SCL -> Arduino Analog (SCL pin)
-  VCC -> Arduino 5V
-  GND -> Arduino GND
-  SQW -> Arduino D2 (Needs to be an interrupt capable pin)
-*/
-
 #include "RTClib.h"     // DS3231 library
 #include <avr/sleep.h>  // AVR library for controlling the sleep modes
-
-const int alarmPin = 2; // The number of the pin for monitoring alarm status on DS3231
-
+#define sensorPin A0
+#define wetSoil 277   // Define max value we consider soil 'wet'
+#define drySoil 380
+const int alarmPin = 2; // Pin for monitoring alarm status on DS3231
+const int buttonPin = 3; // Pin connected to the button
+bool i = false;
+bool j = false;
 RTC_DS3231 rtc;
 
 void setup () {
   Serial.begin(9600); // Start serial port for monitoring
 
   pinMode(alarmPin, INPUT_PULLUP); // Set alarm pin as pullup
-  
-  if (! rtc.begin()) {
+  pinMode(buttonPin, INPUT_PULLUP); // Set button pin as pullup
+
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     abort();
   }
 
-  // If required set time
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // To compiled time
-  //rtc.adjust(DateTime(2020, 7, 3, 20, 0, 0)); // Or explicitly, e.g. July 3, 2020 at 8pm
-
-  // Disable and clear both alarms
   rtc.disableAlarm(1);
   rtc.disableAlarm(2);
   rtc.clearAlarm(1);
   rtc.clearAlarm(2);
 
-  rtc.writeSqwPinMode(DS3231_OFF); // Place SQW pin into alarm interrupt mode
+  rtc.writeSqwPinMode(DS3231_OFF); // Set SQW pin to alarm interrupt mode
 
   Serial.println("Starting");
 }
 
-void loop () {
-
-  delay(10000);  // Wait 10 seconds before going to sleep
-
+void loop() {
+  delay(5000);  // Wait 10 seconds before going to sleep
+  if (!(i)) {Serial.println("button reset");}
   // Get current time and set alarm to a time to wake
   DateTime now = rtc.now();  // Get current time
   rtc.setAlarm1(now + TimeSpan(0, 0, 0, 10), DS3231_A1_Second); // In 10 seconds
-  //rtc.setAlarm1(DateTime(2020, 6, 25, 15, 0, 0), DS3231_A1_Hour); // Or can be set explicity
-    
-  enterSleep();  // Go to sleep
+  
+  enterSleep();  // Go to sleep and wake up as per the alarm or button press
 }
 
 void enterSleep(){
   sleep_enable();                       // Enabling sleep mode
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);  // Setting the sleep mode, in this case full sleep
   
-  noInterrupts();                       // Disable interrupts
-  attachInterrupt(digitalPinToInterrupt(alarmPin), alarm_ISR, LOW);
-  
+  noInterrupts(); // Disable sleep mode when awakened                  // Disable interrupts
+  attachInterrupt(digitalPinToInterrupt(alarmPin), alarm_ISR, LOW); // Attach interrupt for alarm
+  attachInterrupt(digitalPinToInterrupt(buttonPin), button_ISR, FALLING); // Attach interrupt for button press
+  interrupts();
   Serial.println("Going to sleep!");    // Print message to serial monitor
   Serial.flush();                       // Ensure all characters are sent to the serial monitor
   
-  interrupts();                         // Allow interrupts again
+   
   sleep_cpu();                          // Enter sleep mode
+  if (i){
+    i= false;
+    delay(50);
+    Serial.println("button wakeup !");
+      Serial.flush();                       // Ensure all characters are sent to the serial monitor
 
-  /* The program will continue from here when it wakes */
-  
-  // Disable and clear alarm
+  }
+   if (j){
+    j= false;
+    delay(50);
+    Serial.println("RTC wakeup !");
+      Serial.flush();                       // Ensure all characters are sent to the serial monitor
+
+  }
+  sleep_disable();
+
+  // Disable and clear alarm (if awakened by alarm)
   rtc.disableAlarm(1);
   rtc.clearAlarm(1);
-  
-  Serial.println("I'm back!");          // Print message to show we're back
- }
 
+  Serial.println("I'm back!");
+  mesuring()  ;        // Print message to show we're back
+}
 
 void alarm_ISR() {
-  // This runs when SQW pin is low. It will wake up the µController
-  
-  sleep_disable(); // Disable sleep mode
-  detachInterrupt(digitalPinToInterrupt(alarmPin)); // Detach the interrupt to stop it firing
+j = true;
+  detachInterrupt(digitalPinToInterrupt(alarmPin));
 }
+
+void button_ISR() {
+  i = true;
+  detachInterrupt(digitalPinToInterrupt(buttonPin));
+}
+void mesuring(){
+ int moisture = analogRead(sensorPin);
+  Serial.print("Analog output: ");
+  Serial.println(moisture);
+  
+  // Determine status of our soil
+  if (moisture < wetSoil) {
+    Serial.println("Status: Soil is too wet");
+  } else if (moisture >= wetSoil && moisture < drySoil) {
+    Serial.println("Status: Soil moisture is perfect");
+  } else {
+    Serial.println("Status: Soil is too dry - time to water!");
+  }
+  Serial.println();
+}
+
