b90f58b7612edc7cdaa603fd9f6ce3d19854fec5
yassinejemaa <jemmayassin@gmail.com>
2024-05-14 16:44:36 +0200

    temp_moisture_sleep
    node mesures temp, moisture , then sleeps

--- arduini_wakup_watchdog/arduini_wakup_watchdog.ino
+++ arduini_wakup_watchdog/arduini_wakup_watchdog.ino
@@ -1,16 +1,25 @@
+#include <SoftwareSerial.h>
 #include "RTClib.h"     // DS3231 library
 #include <avr/sleep.h>  // AVR library for controlling the sleep modes
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 4
 #define sensorPin A0
 #define wetSoil 277   // Define max value we consider soil 'wet'
 #define drySoil 380
 const int alarmPin = 2; // Pin for monitoring alarm status on DS3231
 const int buttonPin = 3; // Pin connected to the button
+#define RX_PIN 6
+#define TX_PIN 7
 bool i = false;
 bool j = false;
 RTC_DS3231 rtc;
+SoftwareSerial rakSerial(RX_PIN, TX_PIN);
+OneWire  ds(4);
+DallasTemperature sensors(&ds);
 
 void setup () {
-  Serial.begin(9600); // Start serial port for monitoring
+  Serial.begin(115200); // Start serial port for monitoring
 
   pinMode(alarmPin, INPUT_PULLUP); // Set alarm pin as pullup
   pinMode(buttonPin, INPUT_PULLUP); // Set button pin as pullup
@@ -19,6 +28,7 @@ void setup () {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     abort();
+    sensors.begin();
   }
 
   rtc.disableAlarm(1);
@@ -29,6 +39,7 @@ void setup () {
   rtc.writeSqwPinMode(DS3231_OFF); // Set SQW pin to alarm interrupt mode
 
   Serial.println("Starting");
+  rakSerial.begin(115200);
 }
 
 void loop() {
@@ -74,8 +85,8 @@ void enterSleep(){
   rtc.disableAlarm(1);
   rtc.clearAlarm(1);
 
-  Serial.println("I'm back!");
-  mesuring()  ;        // Print message to show we're back
+  Serial.println("I'm back!");       // Print message to show we're back
+  sendmessages(mesuring(),mesuretemp());
 }
 
 void alarm_ISR() {
@@ -87,7 +98,7 @@ void button_ISR() {
   i = true;
   detachInterrupt(digitalPinToInterrupt(buttonPin));
 }
-void mesuring(){
+int mesuring(){
  int moisture = analogRead(sensorPin);
   Serial.print("Analog output: ");
   Serial.println(moisture);
@@ -101,5 +112,42 @@ void mesuring(){
     Serial.println("Status: Soil is too dry - time to water!");
   }
   Serial.println();
+  return moisture;
 }
+int mesuretemp(){
+ // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+  float tempC = sensors.getTempCByIndex(0);
 
+  // Check if reading was successful
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+  } 
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
+return sensors.getTempCByIndex(0);
+
+
+
+}
+void sendmessages(int a , int b ) {
+    // Send "xxx" to RAK4270
+  rakSerial.println("at+version\r\n");
+  if (rakSerial.available()) {
+    // Read the response from RAK4270
+    String response = rakSerial.readStringUntil('\n');
+
+    // Print the response to the Serial monitor
+    Serial.println(response);
+  }
+  delay(250);
+}
